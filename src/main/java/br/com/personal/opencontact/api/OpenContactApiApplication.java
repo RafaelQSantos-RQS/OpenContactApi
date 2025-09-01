856e546923fd8efe4de66e29c3a919e464791865
RafaelQsantos-RQS <rafaelqsantos33@gmail.com>
2025-09-01 01:57:30 -0300

    feat: Initialize Spring Boot project with Maven wrapper
    - Add HELP.md with documentation references and Maven override explanations.
    - Add Maven wrapper script for build automation.
    - Add maven wrapper script for windows.
    - Add Maven project configuration for Spring Boot application.
    - Add the main application class for the OpenContact API.
    - Add spring application name property.
    - Add basic Spring Boot test class for application context loading.

---
+++ src/main/java/br/com/personal/opencontact/api/OpenContactApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.personal.opencontact.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenContactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenContactApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/personal/opencontact/api/OpenContactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personal.opencontact.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenContactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
